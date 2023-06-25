b830aebae5df90decf677f10f3cf21b650aede18
leiloser <2770984447@qq.com>
2023-06-25 22:56:01 +0800

    注释

--- codes/com/volly/StreamRequest.java
+++ codes/com/volly/StreamRequest.java
@@ -10,21 +10,21 @@ import java.io.ByteArrayInputStream;
 import com.android.volley.toolbox.HttpHeaderParser;
 
 public class StreamRequest extends Request<InputStream> {
-   /*
-       time:@2023.6.25
-       author:@leiloser
-       qq:@2913141342
-       QQ-GROU(QQ交流群):@805165262
-       */
-      // 此源码由Android官方提供,StreamRequest.java和StreamRequest.class由我一人编写
-      //开源且免费，谢谢支持
-   
-	private String message;
+	private boolean isChache=false;
+	//private String message;
 	private Response.Listener<InputStream> listener;
     public StreamRequest(int method,String url,Response.Listener<InputStream> listener,Response.ErrorListener erroListener){
 		super(method,url,erroListener);
 		this.listener = listener;
 		
+	}
+
+	public void setIsChache(boolean isChache) {
+		this.isChache = isChache;
+	}
+
+	public boolean isChache() {
+		return isChache;
 	}
 	
 	@Override
@@ -38,16 +38,14 @@ public class StreamRequest extends Request<InputStream> {
 	protected Response<InputStream> parseNetworkResponse(NetworkResponse networkResponse) {
 		InputStream is;
 		is = new ByteArrayInputStream(networkResponse.data);
-		message = "cCache没创建";
-		
+		//message = "Cache没创建";
+		if(!(isChache)){
 		return Response.success(is,null);
-		
+		}else{
+		return Response.success(is,HttpHeaderParser.parseCacheHeaders(networkResponse));
+		}
 	}
 	
     
-    public String getMessage(){
-		
-		return message;
-	}
     
 }
